b8ef9df1320982426c2ed5424759c0f6141d8a83
Raul pallares <rpallares@miners.utep.edu>
2023-03-08 18:31:40 -0700

    blinking red led

--- demos/3-led-blink/main.c
+++ demos/3-led-blink/main.c
@@ -5,8 +5,8 @@
 
 int main(void) {
   P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  P1OUT &= ~LED_RED;
+  //  P1OUT |= LED_RED;
 
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   enableWDTInterrupts();	/* enable periodic interrupt */
@@ -23,7 +23,7 @@ __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
   secondCount ++;
   if (secondCount >= 250) { 	/* once each sec... */
     secondCount = 0;		/* reset count */
-    P1OUT ^= LED_GREEN;		/* toggle green LED */
+    P1OUT ^= LED_RED;		/* toggle RED LED */
   }
 } 
 
