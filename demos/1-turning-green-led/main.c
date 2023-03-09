b7b94ce0acf0f95a8c378f12929739f813dfe274
Raul pallares <rpallares@miners.utep.edu>
2023-03-08 17:12:02 -0700

    turns on green led

--- demos/1-turning-green-led/main.c
+++ demos/1-turning-green-led/main.c
@@ -5,8 +5,8 @@
 
 int main(void) {
   P1DIR |= LEDS;         /*set gate for 65 bits for red and green led */
-  P1OUT &= ~LED_GREEN;   
-  P1OUT |= LED_RED;
-
+  P1OUT &= ~LED_RED    // turning off red led
+  P1OUT |= LED_GREEN;  // turning on green led
+  
   or_sr(0x18);		/* CPU off, GIE on */
 }
